813f6c1b5c3cbe87380b9819256f72a0a2617370
sjv01180 <samuelvelasquez@protonmail.com>
2020-04-19 21:29:43 -0400

    Second Requirement Complete

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 
 })
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const getMain = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+  //fetch from link and store contents (array) into variable
+  const linkArray = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
     .then((res) => {
+      //convert result into json
       return res.json();
+    })
+    .then((result) => {
+      //grab link array from json object
+      return result.variants;
     });
-    console.log(getMain);
-  return new Response('Hello worker', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  
+  //redirect site to first link
+  return Response.redirect(linkArray[0]);
+
+  //redirect site to second link
+  return Response.redirect(linkArray[1]);
 }
