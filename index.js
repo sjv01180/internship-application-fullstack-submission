8940aa48f2d0654a07db64bc78441364a3fd78e5
sjv01180 <samuelvelasquez@protonmail.com>
2020-04-19 20:49:49 -0400

    First Requirement Complete

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
+
 })
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
+  const getMain = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+    .then((res) => {
+      return res.json();
+    });
+    console.log(getMain);
+  return new Response('Hello worker', {
     headers: { 'content-type': 'text/plain' },
   })
 }
